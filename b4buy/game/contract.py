0d59f2f1feead150fd4df5489151b0b3f0891f1b
Robin A. Dorstijn <robbiedorstijn@gmail.com>
2021-06-16 21:20:05 +0200

    Update .gitignore

--- b4buy/game/contract.py
+++ b4buy/game/contract.py
@@ -1,8 +1,14 @@
+from datetime import datetime
+
 class Contract:
-    def __init__(self, player1, player2, end_date, money_transfer, order, frequency):
+    def __init__(self, player1, player2, money_transfer, order, frequency, start_date=None, end_date=None):
         self.player1 = player1
-        self.player2 = player1
-        self.end_date = end_date
+        self.player2 = player2
         self.completed = False
         self.order = order
         self.frequency = frequency
+
+        if not start_date:
+            start_date = datetime.now()
+        self.start_date = start_date
+        self.end_date = end_date
